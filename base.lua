ae322067fbbba06e14f2eb85c199ee48d9398f23
Hesg21as <137196086+Hesg21as@users.noreply.github.com>
2025-03-17 11:42:36 +0500

    мб будет аимботик

--- base.lua
+++ base.lua
@@ -145,6 +145,24 @@ wait(0.3)
    end
 }
 
+
+--[[
+
+кнопка для аима
+
+ldm:Toggle{
+	Name = "Silent aim",
+	StartingState = false,
+	Description = nil,
+	Callback = function(state)
+    
+    end
+}
+
+]]
+
+
+
 --[[
 
 speed
@@ -161,6 +179,8 @@ ldm:Slider{
         game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = nmeq
      end
 }
+
+
 
 
 
@@ -224,4 +244,94 @@ GUI:Credit{
 	V3rm = "None",
 	Discord = "None"
 }
+
+--[[
+
+будущий аим мб
+
+
+local Aiming = loadstring(game:HttpGet("https://pastebin.com/raw/fKyW0Wfx"))()
+Aiming.TeamCheck(false)
+ 
+
+local Workspace = game:GetService("Workspace")
+local Players = game:GetService("Players")
+local RunService = game:GetService("RunService")
+local UserInputService = game:GetService("UserInputService")
+ 
+
+local LocalPlayer = Players.LocalPlayer
+local Mouse = LocalPlayer:GetMouse()
+local CurrentCamera = Workspace.CurrentCamera
+ 
+local DaHoodSettings = {
+    SilentAim = true,
+    AimLock = true,
+    Prediction = 0.165,
+    AimLockKeybind = Enum.KeyCode.E
+}
+getgenv().DaHoodSettings = DaHoodSettings
+ 
+
+function Aiming.Check()
+    
+    if not (Aiming.Enabled == true and Aiming.Selected ~= LocalPlayer and Aiming.SelectedPart ~= nil) then
+        return false
+    end
+ 
+    
+    local Character = Aiming.Character(Aiming.Selected)
+    local KOd = Character:WaitForChild("BodyEffects")["K.O"].Value
+    local Grabbed = Character:FindFirstChild("GRABBING_CONSTRAINT") ~= nil
+ 
+    
+    if (KOd or Grabbed) then
+        return false
+    end
+ 
+    
+    return true
+end
+ 
+
+local __index
+__index = hookmetamethod(game, "__index", function(t, k)
+    
+    if (t:IsA("Mouse") and (k == "Hit" or k == "Target") and Aiming.Check()) then
+        
+        local SelectedPart = Aiming.SelectedPart
+ 
+        
+        if (DaHoodSettings.SilentAim and (k == "Hit" or k == "Target")) then
+            
+            local Hit = SelectedPart.CFrame + (SelectedPart.Velocity * DaHoodSettings.Prediction)
+ 
+            
+            return (k == "Hit" and Hit or SelectedPart)
+        end
+    end
+ 
+    
+    return __index(t, k)
+end)
+ 
+
+RunService:BindToRenderStep("AimLock", 0, function()
+    if (DaHoodSettings.AimLock and Aiming.Check() and UserInputService:IsKeyDown(DaHoodSettings.AimLockKeybind)) then
+        
+        local SelectedPart = Aiming.SelectedPart
+ 
+        
+        local Hit = SelectedPart.CFrame + (SelectedPart.Velocity * DaHoodSettings.Prediction)
+ 
+        
+        CurrentCamera.CFrame = CFrame.lookAt(CurrentCamera.CFrame.Position, Hit.Position)
+    end
+
+]]
+
+
+
+
+
 
